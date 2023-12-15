f75efe666462d5a8bf2e76c1bb4f9e0db8cb65e9
Samuel De La Cruz <samuel@codetiger.co>
2023-12-15 10:22:50 -0400

    finish about_exceptions, finish about_triangle_project

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,19 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  # equilateral: all sides are equal
+  if a == b && b == c
+    return :equilateral
+  end
+  # isosceles: exactly 2 sides are equal
+  if a == b || b == c || a == c
+    return :isosceles
+  end
+  # scalene: no sides are equal
+  if a != b && b != c && a != c
+    return :scalene
+  end
+
 end
 
 # Error class used in part 2.  No need to change this code.
