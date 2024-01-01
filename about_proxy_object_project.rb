e71bc88f82d0b91aa16871427fcc394df0a05b18
Samuel De La Cruz <samuel@codetiger.co>
2024-01-01 13:06:09 -0400

    finish about_proxy_object

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -17,8 +17,25 @@ class Proxy
     @object = target_object
     # ADD MORE CODE HERE
   end
+ 
+  def method_missing(method_name, *args, &block)
+    if @object.respond_to?(method_name)
+      messages << method_name
+      @object.send(method_name, *args, &block)
+    else
+      super(method_name, *args, &block)
+    end
 
-  # WRITE CODE HERE
+  end
+  def messages
+    @messages ||= []
+  end
+  def called?(method_name)
+    messages.include?(method_name)
+  end
+  def number_of_times_called(method_name)
+    messages.count(method_name)
+  end
 end
 
 # The proxy object should pass the following Koan:
